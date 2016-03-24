0b9eb5d907491c4d9259d7291c4236e1d8a0cc93
Mike Dacre <mike.dacre@gmail.com>
2016-03-23 18:59:38 -0700

    Remove python2.7 and 3.2 support, they don't work properly

--- setup.py
+++ setup.py
@@ -33,8 +33,6 @@ setup(
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
